f0751892955b298fadee81356ed7728c1919bffd
Shivam Thaker <Shivamthaker1999@gmail.com>
2021-04-14 21:04:34 +0530

    FaqSection Added

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSectionComponent from "../Components/AboutSectionComponent";
 import ServicesSection from "../Components/ServicesSection";
+import FaqSection from "../Components/FaqSection";
 
 const AboutUS = () => {
   return (
     <div>
       <AboutSectionComponent />
       <ServicesSection />
+      <FaqSection />
     </div>
   );
 };
