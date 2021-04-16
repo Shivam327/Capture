639e3307915e6450fbf42b8e167750e03c8edca5
Shivam Thaker <Shivamthaker1999@gmail.com>
2021-04-16 14:15:25 +0530

    Contact Us Pages Added

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,20 +1,71 @@
 import React from "react";
 //Animation
 import { motion } from "framer-motion";
-import { pageAnimation } from "../Animation";
+import { pageAnimation, titleAnim } from "../Animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       style={{ background: "#fff" }}
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      <h1>Our Contact</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}> Get in Touch</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2> Send Us a Message</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2> Send an Email</h2>
+          </Social>
+        </Hide>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2> Social Media </h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
 
+let ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+let Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+let Hide = styled.div`
+  overflow: hidden;
+`;
+let Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background-color: #353535;
+`;
+let Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 export default ContactUs;
